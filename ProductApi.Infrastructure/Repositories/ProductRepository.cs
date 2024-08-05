83cf0ff711bda8825bd2c66331aff2e84b05f51b
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-05 18:37:24 +0500

    Create ProductApi.Application/Interfaces/IProduct.cs : inherit from IGenericInterface<Product>; Create ProductApi.Infrastructure/Repositories/ProductRepository.cs : inherit from IProduct;

---
+++ ProductApi.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,40 @@
+﻿using eCommerce.SharedLibrary.Responses;
+using ProductApi.Application.Interfaces;
+using ProductApi.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace ProductApi.Infrastructure.Repositories
+{
+    public class ProductRepository : IProduct
+    {
+        public Task<Response> CreateAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> DeleteAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> FindByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByAsync(Expression<Func<Product, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> UpdateAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
