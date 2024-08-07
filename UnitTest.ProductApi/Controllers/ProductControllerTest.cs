61bdb6db1971885ba192fe172dff79f9cd293bbe
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-07 21:03:40 +0500

    Create UnitTest.ProductApi/Controllers/ProductControllerTest.cs : inject IProduct, ProductsController and GET ALL PRODUCTS tests writed; Adding Package to UnitTest.ProductApi : FakeItEasy, FluentAssertions;

---
+++ UnitTest.ProductApi/Controllers/ProductControllerTest.cs
@@ -0,0 +1,77 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ProductApi.Application.DTOs;
+using ProductApi.Application.Interfaces;
+using ProductApi.Domain.Entities;
+using ProductApi.Presentation.Controllers;
+
+namespace UnitTest.ProductApi.Controllers
+{
+    public class ProductControllerTest
+    {
+        private readonly IProduct productInterface;
+        private readonly ProductsController productsController;
+
+        public ProductControllerTest()
+        {
+            // Set up dependencies
+            productInterface = A.Fake<IProduct>();
+
+            // Set up System Under Test - SUT
+            productsController = new ProductsController(productInterface); 
+        }
+
+        // GET ALL PRODUCTS
+        [Fact]
+        public async Task GetProduct_WhenProductExists_ReturnOkResponseWithProducts()
+        {
+            // Arrange
+            var products = new List<Product>()
+            {
+                new() {Id = 1, Name = "Product 1", Price = 100.7m, Quantity = 10},
+                new() {Id = 2, Name = "Product 2", Price = 120.7m, Quantity = 110}
+            };
+
+            // set up fake response for GetAllAsync method
+            A.CallTo(() => productInterface.GetAllAsync()).Returns(products);
+
+            // Act
+            var result = await productsController.GetProducts();
+
+            // Assert
+            var okResult = result.Result as OkObjectResult;
+            okResult.Should().NotBeNull();
+            okResult!.StatusCode.Should().Be(StatusCodes.Status200OK);
+
+            var returnedProducts = okResult.Value as IEnumerable<ProductDTO>;
+            returnedProducts.Should().NotBeNull();
+            returnedProducts.Should().HaveCount(2);
+            returnedProducts!.First().Id.Should().Be(1);
+            returnedProducts!.Last().Id.Should().Be(2);
+        }
+
+        [Fact]
+        public async Task GetProduct_WhenNoProductsExists_ReturnNotFoundResponse()
+        {
+            // Arrange
+            var products = new List<Product>();
+
+            // set up fake response for GetAllAsync method
+            A.CallTo(() => productInterface.GetAllAsync()).Returns(products);
+
+            // Act
+            var result = await productsController.GetProducts();
+
+            // Assert
+            var notFoundResult = result.Result as NotFoundObjectResult;
+            notFoundResult.Should().NotBeNull();
+            notFoundResult!.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+
+            var message = notFoundResult.Value as string;
+            message.Should().Be("No products detected in the database");
+        }
+    }
+}
