80ad6d65d727c58f38181a039beb13a33db3fa2e
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-07 22:12:14 +0500

    UnitTest.ProductApi.Repositories/ProductRepositoryTest.cs :  CREATE PRODUCT tests writed;

--- UnitTest.ProductApi/Repositories/ProductRepositoryTest.cs
+++ UnitTest.ProductApi/Repositories/ProductRepositoryTest.cs
@@ -1,6 +1,8 @@
 ﻿using FakeItEasy;
+using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using ProductApi.Application.Interfaces;
+using ProductApi.Domain.Entities;
 using ProductApi.Infrastructure.Data;
 using ProductApi.Infrastructure.Repositories;
 using ProductApi.Presentation.Controllers;
@@ -20,6 +22,39 @@ namespace UnitTest.ProductApi.Repositories
             productDbContext = new ProductDbContext(options);
 
             productRepository = new ProductRepository(productDbContext);
+        }
+
+        // CREATE PRODUCT
+        [Fact]
+        public async Task CreateAsync_WhenProductAlreadyExist_ReturnErrorResponse()
+        {
+            // arrange 
+            var existingProduct = new Product { Name = "Existing product"};
+            productDbContext.Products.Add(existingProduct);
+            await productDbContext.SaveChangesAsync();
+
+            // Act
+            var result = await productRepository.CreateAsync(existingProduct);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Flag.Should().BeFalse();
+            result.Message.Should().Be("Existing product already added");
+        }
+
+        [Fact]
+        public async Task CreateAsync_WhenProductDoesNotExist_AddProductAndReturnsSuccessResponse()
+        {
+            // arrange 
+            var product = new Product { Name = "Product"};
+
+            // Act
+            var result = await productRepository.CreateAsync(product);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Flag.Should().BeTrue();
+            result.Message.Should().Be("Product added to database successfully!");
         }
     }
 }
