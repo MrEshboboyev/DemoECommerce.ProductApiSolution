fab84a3d2074baa4cd58b96d6161f89198834261
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-07 21:59:23 +0500

    Packages added to UnitTest.ProductApi : Microsoft.EntityFrameworkCore.InMemory; Create UnitTest.ProductApi/Repositories/ProductRepositoryTest.cs : inject ProductDbContext, ProductRepository and constructor : options with 'ProductDb' database name with .UseInMemoryDatabase and passing to ProductDbContext for instance;

---
+++ UnitTest.ProductApi/Repositories/ProductRepositoryTest.cs
@@ -0,0 +1,25 @@
+﻿using FakeItEasy;
+using Microsoft.EntityFrameworkCore;
+using ProductApi.Application.Interfaces;
+using ProductApi.Infrastructure.Data;
+using ProductApi.Infrastructure.Repositories;
+using ProductApi.Presentation.Controllers;
+
+namespace UnitTest.ProductApi.Repositories
+{
+    public class ProductRepositoryTest
+    {
+        private readonly ProductDbContext productDbContext;
+        private readonly ProductRepository productRepository;
+
+        public ProductRepositoryTest()
+        {
+            var options = new DbContextOptionsBuilder<ProductDbContext>()
+                .UseInMemoryDatabase(databaseName: "ProductDb").Options;
+
+            productDbContext = new ProductDbContext(options);
+
+            productRepository = new ProductRepository(productDbContext);
+        }
+    }
+}
